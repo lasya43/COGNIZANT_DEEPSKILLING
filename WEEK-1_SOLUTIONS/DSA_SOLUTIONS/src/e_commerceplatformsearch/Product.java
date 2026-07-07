5e4424f81e7c78c4a00362e4be3e019fc652d329
Lasya Ragi <lasya2006123@gmail.com>
2026-07-07 14:01:23 +0530

    Refactor Product class formatting and structure

--- WEEK-1_SOLUTIONS/DSA_SOLUTIONS/src/e_commerceplatformsearch/Product.java
+++ WEEK-1_SOLUTIONS/DSA_SOLUTIONS/src/e_commerceplatformsearch/Product.java
@@ -4,13 +4,12 @@ class Product {
     int productId;
     String productName;
     String category;
-    // Parametarized Constructor
+    
     public Product(int productId, String productName, String category) {
         this.productId = productId;
         this.productName = productName;
         this.category = category;
     }
-   // @Override
     public String toString() {
         return "Product{" +
                 "productId=" + productId +
